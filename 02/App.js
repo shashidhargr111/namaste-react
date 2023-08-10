9d3bb954acc957e3c13c721860a0ac263bf3b29f
shashidhargr111 <shashidhargr111@gmail.com>
2023-08-10 18:44:08 +0530

    episode 02 files

--- 02/App.js
+++ 02/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello world from React!"
+  [React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello world from React!"
+  ),React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello world from React!🚀"
+  )]
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Namaste React🚀
+
+- Dev build
+- local server
+- HMR HOT MODULE REPLACEMENT
+- File Watching algorithm - written in c++
+- caching - faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent hashing
+- Code spiltting
+- Defferential Bundling - support older browsers
